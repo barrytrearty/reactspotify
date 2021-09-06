0de3870b6c3ed73a4ff3e335b19825bd0f227ce0
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-09-06 10:47:02 +0100

    Fixed scrollbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
           <li className="nav-item mb-2 text-white">
             <img
               className="pl-2 pt-3"
-              src="./logo/spotifiy.png"
+              src="..\..\data\spotifiy.png"
               height="40px"
               width="auto"
               alt=""
@@ -14,7 +14,7 @@ const Sidebar = () => {
             />
           </li>
           <li className="nav-item text-white pl-2 activehome">
-            <h6>
+            <p>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="14"
@@ -26,10 +26,10 @@ const Sidebar = () => {
                 <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z" />
               </svg>{" "}
               <a href="#Details">Home</a>
-            </h6>
+            </p>
           </li>
           <li className="nav-item text-white">
-            <h6>
+            <p>
               <a href="#Details">
                 <input
                   id="input"
@@ -50,10 +50,10 @@ const Sidebar = () => {
                   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                 </svg>
               </a>
-            </h6>
+            </p>
           </li>
           <li className="nav-item text-white ml-2 mb-4">
-            <h6>
+            <p>
               {" "}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -70,10 +70,10 @@ const Sidebar = () => {
                 <path d="M6 13a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v10z" />
               </svg>{" "}
               <a href="#Details">Your Library</a>{" "}
-            </h6>
+            </p>
           </li>
           <li className="nav-item text-white ml-2">
-            <h6>
+            <p>
               {" "}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -86,10 +86,10 @@ const Sidebar = () => {
                 <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z" />
               </svg>
               <a href="#Details"> Create Playlist</a>
-            </h6>
+            </p>
           </li>
           <li className="nav-item text-white ml-2 mb-0">
-            <h6>
+            <p>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="14"
@@ -104,70 +104,70 @@ const Sidebar = () => {
                 />
               </svg>
               <a href="#Details"> Liked Songs</a>
-            </h6>
+            </p>
           </li>
         </ul>
         {/* <hr style={"color: rgb(102, 102, 102)"} /> */}
         <div className="pl-2 pb-5 mb-5 scrollbar-list">
-          <a className="click-me" href="#Details">
-            <h6>Funky Heavy Bluesy</h6>
+          <a className="click-me" href="#Album">
+            <p>Funky Heavy Bluesy</p>
           </a>
-          <a className="click-me " href="#Details">
-            <h6>Your Top Songs 2020</h6>
-          </a>
-
-          <a className="click-me " href="#Details">
-            <h6>In Love With You-</h6>
+          <a className="click-me " href="#Album">
+            <p>Your Top Songs 2020</p>
           </a>
 
-          <a className="click-me" href="#Details">
-            <h6>This Is Rod Stewart</h6>
+          <a className="click-me " href="#Album">
+            <p>In Love With You-</p>
           </a>
 
-          <a className="click-me" href="#Details">
-            <h6>FRANCHISE ft.Young Thug...</h6>
+          <a className="click-me" href="#Album">
+            <p>This Is Rod Stewart</p>
           </a>
 
-          <a className="click-me" href="#Details">
-            <h6>Your Top Songs 2019</h6>
-          </a>
-          <a className="click-me" href="#Details">
-            <h6>Palleggio</h6>
-          </a>
-          <a className="click-me" href="#Details">
-            <h6>This is Joe Bonamassa</h6>
+          <a className="click-me" href="#Album">
+            <p>FRANCHISE ft.Young Thug...</p>
           </a>
 
-          <a className="click-me" href="#Details">
-            <h6>Jun19</h6>
+          <a className="click-me" href="#Album">
+            <p>Your Top Songs 2019</p>
+          </a>
+          <a className="click-me" href="#Album">
+            <p>Palleggio</p>
+          </a>
+          <a className="click-me" href="#Album">
+            <p>This is Joe Bonamassa</p>
           </a>
 
-          <a className="click-me" href="#Details">
-            <h6>Jack 30th Party 2</h6>
+          <a className="click-me" href="#Album">
+            <p>Jun19</p>
           </a>
 
-          <a className="click-me" href="#Details">
-            <h6>Jack 30th Party</h6>
-          </a>
-          <a className="click-me" href="#Details">
-            <h6>This is Opeth</h6>
-          </a>
-          <a className="click-me" href="#Details">
-            <h6>Matal Ballads</h6>
-          </a>
-          <a className="click-me" href="#Details">
-            <h6>New Playlist</h6>
+          <a className="click-me" href="#Album">
+            <p>Jack 30th Party 2</p>
           </a>
 
-          <a className="click-me" href="#Details">
-            <h6>You Top Songs 2018</h6>
+          <a className="click-me" href="#Album">
+            <p>Jack 30th Party</p>
+          </a>
+          <a className="click-me" href="#Album">
+            <p>This is Opeth</p>
+          </a>
+          <a className="click-me" href="#Album">
+            <p>Matal Ballads</p>
+          </a>
+          <a className="click-me" href="#Album">
+            <p>New Playlist</p>
           </a>
 
-          <a className="click-me" href="#Details">
-            <h6>Jazz Classics Blune Note Editi...</h6>
+          <a className="click-me" href="#Album">
+            <p>You Top Songs 2018</p>
           </a>
 
-          <h6 className="text-white ml-3 fixedLink">
+          <a className="click-me" href="#Album">
+            <p>What People Say I Am, That Is What I Am Not</p>
+          </a>
+
+          <p className="text-white ml-3 fixedLink">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -181,11 +181,14 @@ const Sidebar = () => {
             <a className="text-white" href="#Details">
               Install App
             </a>
-          </h6>
+          </p>
         </div>
       </div>
     </div>
   );
 };
+
+//Get rid of ps
+//Have each link go to a different album page
 
 export default Sidebar;
